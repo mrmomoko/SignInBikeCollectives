106c1c8a45b35471b104375bfa3921872a8bbaa6
Momoko <momoko@thinkfishhook.com>
2014-12-28 09:09:22 -0800

    Initial Commit

---
+++ SignIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignIn
+//
+//  Created by Momoko Saunders on 12/28/14.
+//  Copyright (c) 2014 Momoko Saunders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SignIn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SignIn
+//
+//  Created by Momoko Saunders on 12/28/14.
+//  Copyright (c) 2014 Momoko Saunders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
